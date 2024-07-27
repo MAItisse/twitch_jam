f90b9bf5e2d9efc532e48f041d3bfb3b233eac76
vivax <vivax3794@protonmail.com>
2024-07-27 20:39:56 +0200

    feat: Project setup and dockerfile for server

---
+++ server/README.md
@@ -0,0 +1,4 @@
+# Run Server
+```bash
+docker compose up --build
+```
---
+++ server/src/main.rs
@@ -0,0 +1,39 @@
+#![warn(
+    clippy::pedantic,
+    clippy::clone_on_ref_ptr,
+    clippy::create_dir,
+    clippy::filetype_is_file,
+    clippy::fn_to_numeric_cast_any,
+    clippy::if_then_some_else_none,
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    clippy::missing_const_for_fn,
+    clippy::mixed_read_write_in_expression,
+    clippy::panic,
+    clippy::partial_pub_fields,
+    clippy::same_name_method,
+    clippy::str_to_string,
+    clippy::suspicious_xor_used_as_pow,
+    clippy::try_err,
+    clippy::unneeded_field_pattern,
+    clippy::use_debug,
+    clippy::verbose_file_reads,
+    clippy::expect_used
+)]
+#![deny(
+    clippy::unwrap_used,
+    clippy::unreachable,
+    clippy::unimplemented,
+    clippy::todo,
+    clippy::dbg_macro,
+    clippy::error_impl_error,
+    clippy::exit,
+    clippy::panic_in_result_fn,
+    clippy::tests_outside_test_module
+)]
+
+fn main() {
+    println!("Hello Docker!");
+}
