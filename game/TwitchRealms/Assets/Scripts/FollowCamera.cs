b5f4981d0e6d02bd0fb56f0c611ebd93f5417b97
Matisse <MatissesProjects@users.noreply.github.com>
2024-08-04 06:47:38 -0700

    fix(game): moving vector for camera on change

--- game/TwitchRealms/Assets/Scripts/FollowCamera.cs
+++ game/TwitchRealms/Assets/Scripts/FollowCamera.cs
@@ -14,11 +14,6 @@ public class FollowCamera : MonoBehaviour
     private Vector3 currentVelocity;
     private bool rotateOnSpace = false;
 
-    void Start()
-    {
-        currentOffset = offset;
-    }
-
     void Update()
     {
         HandleCameraFollow();
@@ -32,7 +27,7 @@ public class FollowCamera : MonoBehaviour
 
     void HandleCameraFollow()
     {
-        Vector3 desiredPosition = followMe.position + currentOffset;
+        Vector3 desiredPosition = followMe.position + offset;
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref currentVelocity, followSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
