8e0ad4f5d68bb633da02bc977de84287598c339b
RGBProductions <rgb@rgbproductions.dev>
2024-08-04 16:37:46 -0700

    feat(game): added bubble spawner

---
+++ game/TwitchRealms/Assets/Scripts/BubbleData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BubbleData
+{
+    public float x = 0f;
+    public float y = 0f;
+    public string bubbleColor = "#00FF00";
+    public float bubbleSize = 50;
+    public string userId = null;
+}
