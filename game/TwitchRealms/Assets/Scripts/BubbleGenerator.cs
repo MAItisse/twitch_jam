97a45795a4947efb507c80c84c7781a341b188d0
RGBProductions <luvm1n3craft@gmail.com>
2024-08-04 17:00:35 -0700

    fix(game): bubble uses existing component

--- game/TwitchRealms/Assets/Scripts/BubbleGenerator.cs
+++ game/TwitchRealms/Assets/Scripts/BubbleGenerator.cs
@@ -10,7 +10,7 @@ public class BubbleGenerator : MonoBehaviour
     public void SpawnBubble(string color, float size, Vector2 position) {
         Vector3 spawnPosition = transform.position + new Vector3(position.x, 0, position.y);
         GameObject bubble = Instantiate(bubblePrefab, spawnPosition, Quaternion.identity, transform.parent);
-        Bubble bubbleComp = bubble.AddComponent<Bubble>();
+        Bubble bubbleComp = bubble.GetComponent<Bubble>();
         bubbleComp.size = size;
         Material material = new Material(baseMaterial);
         Color matColor = new Color(0,0,0);
