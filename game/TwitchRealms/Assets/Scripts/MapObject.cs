ea88a474280f4b2eeb692d114c24ae4adf0bef12
Matisse <MatissesProjects@users.noreply.github.com>
2024-08-17 17:15:31 -0700

    feat(game): on enter trigger remove extraCss

--- game/TwitchRealms/Assets/Scripts/MapObject.cs
+++ game/TwitchRealms/Assets/Scripts/MapObject.cs
@@ -6,4 +6,12 @@ public class MapObject : MonoBehaviour
 {
     public Color mapColor = Color.white;
     public string extraCss = "";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.TryGetComponent<Combinable>(out var combinable))
+        {
+            extraCss = "";
+        }
+    }
 }
